b9c45a17909df3144b850ab592f7af389f116cff
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-03 09:49:30 +0700

    setup module exercise project

---
+++ devplus/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app/stylesheet/style.scss'
+import Header from './app/components/header/Header';
+
+function App() {
+  return (
+    <Header/>
+  );
+}
+
+export default App;

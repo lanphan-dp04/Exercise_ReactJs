0cb49f7f6e5242044e8603f73efdd40b26278133
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-08 10:31:21 +0700

    done feature/common-concerns

--- devplus/src/App.js
+++ devplus/src/App.js
@@ -1,11 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './app/stylesheet/style.scss'
-import Header from './app/components/header/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./app/stylesheet/style.scss";
+import Header from "./app/components/header/Header";
+import Main from "./app/components/main/Main";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
-    <Header/>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
 
---
+++ devplus/src/app/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Concerns from "./concerns/Concerns";
+
+const Main = () => {
+  return (
+    <>
+      <Concerns />
+    </>
+  );
+};
+
+export default Main;

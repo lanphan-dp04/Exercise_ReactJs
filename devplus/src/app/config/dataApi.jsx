8093f50d40715cad885ed5879bae74d6bc71f036
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-11-23 11:17:16 +0700

    feature/alumni-saying

--- devplus/src/app/config/dataApi.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const dataSildeBar = () => axios.get('https://6361ddfc7521369cd05fab83.mockapi.io/api/slideBar');
-
-export const dataCommonConcern = () => axios.get('https://6361ddfc7521369cd05fab83.mockapi.io/api/commonConcerns');
-
-export const dataLumniSaying = () => axios.get('https://6361ddfc7521369cd05fab83.mockapi.io/api/alumniSaying');
